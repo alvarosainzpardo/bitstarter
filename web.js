aa05f9c26d4dd7bbebe1066986187fb983cb1a78
Alvaro Sainz-Pardo <alvaro.sainzpardo@gmail.com>
2013-07-14 22:59:23 +0200

    web.js updated to read index.html

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile('index.html', function (err, data) {
+  		if (err)
+			response.send('Hello World 2!');
+  		else
+	  		response.send(data.toString());  			
+	});
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
