8f7f1ccad4bc70189c3d495a7efce926f77fd27d
Alvaro Sainz-Pardo <alvaro.sainzpardo@gmail.com>
2013-07-21 16:46:40 +0200

    init

---
+++ web.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	fs.readFile('index.html', function (err, data) {
+  		if (err)
+			response.send('Hello World 2!');
+  		else
+	  		response.send(data.toString());  			
+	});
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
